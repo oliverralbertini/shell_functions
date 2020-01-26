c6e3397cba96e637763c720aac02fdee1859040e
Oliver Albertini <oalbertini@pivotal.io>
2020-01-26 09:55:17 -0800

    Refactor fuzzpass
    Authored-by: Oliver Albertini <oalbertini@pivotal.io>

--- fuzzpass.sh
+++ fuzzpass.sh
@@ -1,20 +1,69 @@
 #!/usr/bin/env bash
+# mkdir -p ~/.fuzzpass
+
+get_gpg_files() {
+	local PASSWORD_STORE=${PASSWORD_STORE:-~/.password-store}
+	find "${PASSWORD_STORE}" -name '*.gpg' |
+		sed -E "s,(.*)${PASSWORD_STORE##*/}/(.*)\.gpg,\2,"
+}
 
 fuzzpass() {
-	local path
+	local path edit_path \
+		create='create a new password' \
+		push='push changes' \
+		pull='pull changes' \
+		edit='edit a password' \
+		generate='generate a new password' \
+		shell_session='start a shell session for pass'
 	while true; do
 		path=$(
-			find ~/.password-store -name '*.gpg' | \
-			sed -E 's,(.*)\.password-store/(.*)\.gpg,\2,' | \
-			fzf +m
+		{
+			get_gpg_files
+			printf '%s\n%s\n%s\n%s\n%s\n%s' \
+				"$(tput sitm)$(tput setaf 2)${create}" \
+				"${push}" \
+				"${pull}" \
+				"${generate}" \
+				"${shell_session}" \
+				"$(tput setaf 1)${edit}"
+		} |
+			fzf +m --border \
+			--prompt='choose a password or action (^D to quit): ' \
+			--no-clear \
+			--ansi
 		)
-		[[ -z ${path} ]] && return
-		if [[ ${path} =~ otp-key ]]; then
-			local pin
-			pin=$(oathtool --base32 --totp "$(pass "${path}")")
-			printf '%s' "${pin}" | pbcopy
-			continue
-		fi
+		[[ -z ${path} ]] && { clear; return; }
+		case $path in
+			"${create}"|"${shell_session}"|"${generate}")
+				clear
+				PS1='pass-> ' bash --init-file ~/workspace/shell_functions/completion.d/pass.sh
+				continue
+				;;
+			"${edit}")
+				edit_path=$(
+					get_gpg_files |
+					fzf +m --border \
+					--prompt='choose a password to edit (^D to quit): ' \
+					--no-clear \
+					--ansi
+				)
+				pass edit "${edit_path}"
+				continue
+				;;
+			"${push}")
+				pass git pull --rebase
+				pass git push
+				continue
+				;;
+			"${pull}")
+				pass git pull --rebase
+				continue
+				;;
+			*otp-key)
+				printf '%s' "$(oathtool --base32 --totp "$(pass "${path}")")" | pbcopy
+				continue
+				;;
+		esac
 		pass -c "${path}"
 	done
 }
